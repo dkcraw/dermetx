04fbd147c4e91fa0965a834a760c43da878d1e46
dkcraw <dcrawford@etxitsolutions.com>
2026-02-16 11:41:08 -0600

    Add files via upload

--- components/hours.tsx
+++ components/hours.tsx
@@ -61,8 +61,7 @@ export function Hours() {
 
             <div className="mb-8 aspect-video overflow-hidden rounded-sm bg-muted">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3440.5!2d-94.8496!3d31.3382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2z107+Christie+St%2C+Lufkin%2C+TX+75904!5e0!3m2!1sen!2sus!4v1700000000000"
-                width="100%"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3440.5!2d-94.8496!3d31.3382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTHCsDIwJzE3LjUiTiA5NMKwNTAnNTguNiJX!5e0!3m2!1sen!2sus!4v1739512345678!5m2!1sen!2sus"                width="100%"
                 height="100%"
                 style={{ border: 0 }}
                 allowFullScreen
